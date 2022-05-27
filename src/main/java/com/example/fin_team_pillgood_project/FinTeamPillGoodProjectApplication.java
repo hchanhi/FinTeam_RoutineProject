6a382cb0baa370e1417653bfad441bfcca8d92e5
Hchan <hchani95@gamil.com>
2022-05-27 14:46:52 +0900

    [add] project setting

---
+++ src/main/java/com/example/fin_team_pillgood_project/FinTeamPillGoodProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fin_team_pillgood_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTeamPillGoodProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinTeamPillGoodProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fin_team_pillgood_project/FinTeamPillGoodProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fin_team_pillgood_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTeamPillGoodProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
