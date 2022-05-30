ff36152cb58145aaec6efb40f445b276caf82e56
Minhyuk_Lee <kazesky93@gmail.com>
2022-05-30 17:26:44 +0900

    [modify]add form

--- frontend/src/alarm/AddPill.js
+++ frontend/src/alarm/AddPill.js
@@ -7,8 +7,15 @@ function AddPill(){
             <br/>
             <br/>
             <br/>
-            <h1>약추가 페이지</h1>
-
+            <form>
+                <h1>영양제 등록하기</h1>
+                <span>영양제 이름</span><input/><br/>
+                <span>총 수량(현재수량)</span><input/><br/>
+                <span>1회 복용량</span><input/><br/>
+                <span>복용 시간</span><input/><br/>
+                <span>복용 주기</span><input/><br/>
+                <button type='submit'>등록</button>
+            </form>
         </div>
     );
 }
