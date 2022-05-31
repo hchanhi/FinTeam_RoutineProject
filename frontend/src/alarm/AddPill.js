6f8d63610752123ce4ffdd9eba6b3d4bfdcfb409
Minhyuk_Lee <kazesky93@gmail.com>
2022-05-31 16:06:15 +0900

    [add]요일check기능

--- frontend/src/alarm/AddPill.js
+++ frontend/src/alarm/AddPill.js
@@ -1,24 +1,52 @@
 import {Form, Card, Button} from "react-bootstrap";
 import './AddPill.css';
 import {Link} from "react-router-dom";
-import CheckBox from './CheckBox.js';
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
+import {useState} from "react";
 
 function AddPill(){
 
     let navigate = useNavigate();
+    let [pillname, setPillname] = useState('');
+    let [nownumber, setNownumber] = useState(0);
+    let [eatnumber, setEatnumber] = useState(0);
+    let [eattime1, setEattime1] = useState('');
+    let [eattime2, setEattime2] = useState('');
+    let [eattime3, setEattime3] = useState('');
+    let [checkday, setCheckday] = useState('');
+    let [eatday, setEatday]=useState('');
+    let [week, setWeek] = useState(['월', '화', '수', '목', '금', '토', '일']);
 
-    let data = [];
+    function selectDay(e){
+        let copy = [...checkday,e];
+        setCheckday(copy);
+        setEatday(checkday);
+    }
+    function unselectDay(e){
+        let copy = [...checkday];
+        for(let i=0; i<copy.length;i++){
+            if(copy[i]==e){
+                copy.splice(i,1);
+                i--;
+            }
+        }
+        setCheckday(copy);
+        setEatday(checkday);
+    }
+
+    let body ={name: pillname, nownumber:nownumber, eatnumber:eatnumber, eattime1:eattime1, eattime2:eattime2, eattime3:eattime3,
+    eatday: eatday }
 
     function addPill(){
-        axios.post('', data)
-            .then(function(res){
-
-        })
-            .catch(function(res){
-
-        })
+        console.log(body);
+        // axios.post('', data)
+        //     .then(function(res){
+        //
+        // })
+        //     .catch(function(res){
+        //
+        // })
     }
 
     return(
@@ -31,14 +59,25 @@ function AddPill(){
                     <Card.Title>영양제 등록하기</Card.Title>
                     <div>
                         <Form>
-                            <span>영양제 이름</span><input type="text" className="cardInput" /><br/><br/>
-                            <span>총 수량(현재수량)</span><input type="number" className="cardInput" /><br/><br/>
-                            <span>1회 복용량</span><input type="number" className="cardInput" /><br/><br/>
-                            <span>복용 시간1</span><input type="time" className="cardInput"/><br/><br/>
-                            <span>복용 시간2</span><input type="time" className="cardInput"/><br/><br/>
-                            <span>복용 시간3</span><input type="time" className="cardInput"/><br/><br/>
+                            <span>영양제 이름</span><input type="text" className="cardInput" onChange={(e)=>{setPillname(e.target.value)}}/><br/><br/>
+                            <span>총 수량(현재수량)</span><input type="number" className="cardInput" onChange={(e)=>{setNownumber(e.target.value)}}/><br/><br/>
+                            <span>1회 복용량</span><input type="number" className="cardInput" onChange={(e)=>{setEatnumber(e.target.value)}}/><br/><br/>
+                            <span>복용 시간1</span><input type="time" className="cardInput" onChange={(e)=>{setEattime1(e.target.value)}}/><br/><br/>
+                            <span>복용 시간2</span><input type="time" className="cardInput" onChange={(e)=>{setEattime2(e.target.value)}}/><br/><br/>
+                            <span>복용 시간3</span><input type="time" className="cardInput" onChange={(e)=>{setEattime3(e.target.value)}}/><br/><br/>
                             <span>복용 주기</span>
-                                <CheckBox/>
+                            <div className="mb-3">
+                                {week.map(function (week,index){
+                                    return(
+                                        <div className="checkbox_area">
+                                        <span style={{margin:'15px'}}>{week}</span>
+                                        <input key={index} type='checkbox' id={week} value={week} onChange={(e)=>
+                                        { if(e.target.checked){selectDay(e.target.value)}
+                                        else {unselectDay(e.target.value)}}}/>
+                                        </div>
+                                    );
+                                })}
+                            </div>
                             <br/>
 
                         </Form>
@@ -47,7 +86,7 @@ function AddPill(){
             </Card>
             <br/>
             <div className="btnArea">
-                <Button variant="primary" className="cardBtn" onClick={addPill()}>등 록</Button>
+                <Button variant="primary" className="cardBtn" onClick={addPill}>등 록</Button>
                 <Button variant="danger" className="cardBtn" onClick={()=>{navigate(-1)}}>취 소</Button>
             </div>
         </div>
