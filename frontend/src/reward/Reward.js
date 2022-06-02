e778cf68b676eb93c7219a2d31b4831d1492f922
CODE0000 <thfxm0601@gmail.com>
2022-06-02 11:58:51 +0900

    [modify]/ 리워드 css 수정
    [modify]/ 리워드 css 수정

--- frontend/src/reward/Reward.js
+++ frontend/src/reward/Reward.js
@@ -1,42 +1,97 @@
 import { useState } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAward, faHouse, faCapsules, faArrowsRotate, faUserGear } from "@fortawesome/free-solid-svg-icons";
+
+let Badge = styled.div`
+
+img{
+    width: 150px;
+height: 150px;
+
+}
+
+
+`;
+let Wrapper = styled.div`
+
+display: flex;
+flex-direction: column;
+flex-wrap: wrap;
+width: 30px;
+height: 200px;
+margin-left:20px
+
+
+
+`;
+let WrapperBg = styled.div`
+
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+width: 500px;
+justify-content:center
+
+
+
+`;
+let Text = styled.div`
+
+    margin-left:20px
+
+
+
+
+`;
 
 function Reward() {
 
 
-    let [bd, setBd] = useState([true, false, true, false, true]);
+    let [bd, setBd] = useState([true, false, true, true, true, false, false, false, false]);
     let [rt, setRt] = useState([1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]);
     return (
-        <div>
+        <div >
             <br /><br /><br />
-            <h1>리워드</h1>
-            <h1>연속</h1>
+            <Text>
+                <h4>리워드 <FontAwesomeIcon style={{ color: "black" }} className='icon' icon={faAward} /></h4>
+                <br />
+                <h3>루틴 필드</h3>
+            </Text>
 
+            <div >
+                <Wrapper >
+                    {
+                        rt.map(function (a, i) {
+                            return (
+                                <h1 key={i}>{rt[i] == 1 ? <div style={{ width: '30px', height: '30px', backgroundColor: 'skyblue', marginLeft: '10px' }}></div> :
+                                    <div style={{ width: '30px', height: '30px', backgroundColor: 'grey', marginLeft: '10px' }}></div>}</h1>
+                            );
+                        })
 
-            <div style={{ display: 'flex', width: "90%" }}>
-                {
-                    rt.map(function (a, i) {
-                        return (
-                            <h1>{rt[i] == 1 ? <div style={{ width: '30px', height: '30px', backgroundColor: 'skyblue', marginLeft: '10px' }}></div> :
-                                <div style={{ width: '30px', height: '30px', backgroundColor: 'grey', marginLeft: '10px' }}></div>}</h1>
-                        );
-                    })
-
-                }
+                    }
+                </Wrapper>
             </div>
+            <Text>
+                <h3>뱃지</h3>
+            </Text>
 
-            <h1>뱃지</h1>
-            <div style={{ display: "flex", alignItems: 'center', width: "100px", height: "200px", textAlign: 'center', color: 'red' }}>
-                {
-                    bd.map(function (a, i) {
-                        return (
-                            <h1>{bd[i] == true ? "🎃" : "땡"}</h1>
-                        );
-                    })
+            <div style={{ width: "300px", textAlign: "center" }}>
+                <WrapperBg>
+                    {
+                        bd.map(function (a, i) {
+                            return (
+                                <h1 key={i}>{bd[i] == true ? <Badge><img src={require("../img/badge00" + (i + 1) + ".png")} /></Badge>
+                                    : <Badge><img src={require("../img/qBadge.png")} /></Badge>}</h1>
 
-                }
+                            );
+                        })
+
+                    }
+                </WrapperBg>
+
             </div>
-
-
+            <br /><br /><br />
         </div>
     );
 }
