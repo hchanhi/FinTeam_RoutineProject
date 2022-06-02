db56dbe79532fbd4de840e2024c52a682f9bce21
Hchan <hchani95@gamil.com>
2022-06-02 10:15:53 +0900

    [modify] fcm server setting

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
-import("https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js").then(
-    (firebase) => {
-        const firebaseConfig = {
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+
+        firebase.initializeApp({
             apiKey: "AIzaSyApcyEQg322SpdhimM9wMfQLuIn90BDZT4",
             authDomain: "pillgood-fa622.firebaseapp.com",
             projectId: "pillgood-fa622",
@@ -8,8 +9,20 @@ import("https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js").then(
             messagingSenderId: "643765049046",
             appId: "1:643765049046:web:9e4be72f3998028de45b77",
             measurementId: "G-95F883RD6Z"
-        };
-        const app = firebase.initializeApp(firebaseConfig);
-        //...
-    });
+        });
+        const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon:''
+    };
+
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
 
