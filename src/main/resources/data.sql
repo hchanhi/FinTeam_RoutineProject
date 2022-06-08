cadde6f74119c0767190ebc71ac6cc9d8a97e6c3
Hayoung Yu <nhy8001@gmail.com>
2022-06-09 01:38:31 +0900

    [add] add record data
    더미데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,6 +10,18 @@ insert into users(email, created_date, modified_date, nickname, password) values
 insert into users(email, created_date, modified_date, nickname, password) values("dddd@dddd.com", "2022-06-01 00:00:00", "2022-06-01 00:00:00", "신주", "$2a$10$SF8DLsuB42mCZ6IwagbuZeTWWSejLc7MQf1E9xG5Xqmb6ScmQG1oG");
 insert into users(email, created_date, modified_date, nickname, password) values("eeee@eeee.com", "2022-06-01 00:00:00", "2022-06-01 00:00:00", "구주", "$2a$10$SF8DLsuB42mCZ6IwagbuZeTWWSejLc7MQf1E9xG5Xqmb6ScmQG1oG");
 
+-- 레코드
+insert into record (id, created_date, modified_date, continuity, max_continuity, user_id) values (1, "2022-06-01 00:00:00", "2022-06-01 00:00:00", 0, 0, 1);
+insert into record (id, created_date, modified_date, continuity, max_continuity, user_id) values (2, "2022-06-01 00:00:00", "2022-06-01 00:00:00", 0, 0, 2);
+insert into record (id, created_date, modified_date, continuity, max_continuity, user_id) values (3, "2022-06-01 00:00:00", "2022-06-01 00:00:00", 0, 0, 3);
+insert into record (id, created_date, modified_date, continuity, max_continuity, user_id) values (4, "2022-05-25 00:00:00", "2022-05-25 00:00:00", 0, 0, 4);
+insert into record (id, created_date, modified_date, continuity, max_continuity, user_id) values (5, "2022-06-01 00:00:00", "2022-06-01 00:00:00", 0, 0, 5);
+insert into record (id, created_date, modified_date, continuity, max_continuity, user_id) values (6, "2022-06-01 00:00:00", "2022-06-01 00:00:00", 0, 0, 6);
+insert into record (id, created_date, modified_date, continuity, max_continuity, user_id) values (7, "2022-06-01 00:00:00", "2022-06-01 00:00:00", 0, 0, 7);
+
+
+
+
 -- 뱃지
 insert into achievements (id, title, description, needs_count) values (1, 'pillgood', '첫 로그인', 0);
 insert into achievements (id, title, description, needs_count) values (2, 'supplements_count', '영양제 1개 등록', 1);
